43e2ca5a3abb4f12b29baf709f946ca2b4848203
ai-abhishekdey <ai.abhishekdey@gmail.com>
2026-02-14 23:08:21 +0530

    init upload

---
+++ README.md
@@ -0,0 +1,101 @@
+##  Clinical Voice Assistant with End-to-End Speech Intelligence
+
+**Author: Abhishek Dey**
+
+## Problem Statement:
+
+To build a real-time voice agent in English Language who is checking with a patient, regarding the post surgery followup.   Patient had Ankle surgery after a fall from a horse while playing Polo and is enrolled into a home program for recovery.
+
+- Agent has to perform the following checkin related questions (a to e questions) in sequential manner:
+  a. Introduce yourself about why you called the patient.
+  b. Ask about how the patient is feeling after surgery
+  c. Assess the pain score (1-10)
+  d. Guide the patient to work on the following exercises one by one (to have faster recovery):
+   - Ankle Mobility Stretch
+   - Toe Tapping
+   - Calf Raises.
+   - After explaining instructions to do the above exercises, ask: “Would you like to take a moment to go through the steps for [exercise name] now? and tell
+that you can hold/pause the call upto one minute.
+  e. Call closing
+
+## Voice Agent Development:
+
+The voice agent is developed using **pipecat** framework considering the following AI Model services
+
+1. **Deepgram** for ASR
+2. **Cartesia** for TTS
+3. **OpenAI** for LLM
+4. **Silero-vad** for Voice  Activity Detection
+
+## Getting Started:
+---
+## Get API Keys:
+
+1. Get Deepgram API from [here](https://console.deepgram.com/signup)
+2. Get OpenAI API Key from [here](https://auth.openai.com/create-account)
+3. Get Cartesia API Key from [here](https://play.cartesia.ai/sign-up)
+
+## Configure your API keys
+
+* Copy the sample environment file as .env
+
+```
+cp env.example .env
+```
+
+* Open the .env file in your text editor and add your API keys:
+
+```
+DEEPGRAM_API_KEY=your_deepgram_api_key
+OPENAI_API_KEY=your_openai_api_key
+CARTESIA_API_KEY=your_cartesia_api_key
+```
+
+## Set up virtual environment and install dependencies
+
+```
+uv sync
+
+```
+
+## Run your bot locally
+
+```
+uv run bot.py
+```
+
+* You should see output similar to this:
+
+```
+🚀 Bot ready!
+   → Open http://localhost:7860/client in your browser
+
+INFO:     Started server process [55397]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+INFO:     Uvicorn running on http://localhost:7860 (Press CTRL+C to quit)
+```
+## Note: 
+
+* To check the process using port: 7860
+
+```
+lsof -i :7860
+```
+
+* To kill the process
+
+```
+kill -9 <PID>
+```
+
+
+## References:
+
+1. [Pipecat Documentation](https://docs.pipecat.ai/getting-started/introduction)
+2. [Pipecat QuickStart](https://github.com/pipecat-ai/pipecat-quickstart)
+3. [Cartesia](https://cartesia.ai/sonic)
+4. [Deepgram](https://developers.deepgram.com/docs/pre-recorded-audio)
+5. [OpenAI](https://openai.com/)
+6. [Silero-vad](https://github.com/snakers4/silero-vad)
+
---
+++ bot.py
@@ -0,0 +1,166 @@
+"""
+Health Care Voice Agent
+Required AI services:
+- Deepgram (Speech-to-Text)
+- OpenAI (LLM)
+- Cartesia (Text-to-Speech)
+
+Run the bot using::
+
+    uv run bot.py
+"""
+
+import os
+from dotenv import load_dotenv
+from loguru import logger
+
+print("Starting Voice Agent...")
+print("Loading models and imports (20 seconds, first run only)\n")
+
+logger.info("Loading Local Smart Turn Analyzer V3...")
+from pipecat.audio.turn.smart_turn.local_smart_turn_v3 import LocalSmartTurnAnalyzerV3
+
+logger.info("Local Smart Turn Analyzer V3 loaded")
+logger.info("Loading Silero VAD model...")
+from pipecat.audio.vad.silero import SileroVADAnalyzer
+
+logger.info("Silero VAD model loaded")
+
+from pipecat.audio.vad.vad_analyzer import VADParams
+from pipecat.frames.frames import LLMRunFrame
+
+logger.info("Loading pipeline components...")
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.pipeline.runner import PipelineRunner
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.llm_context import LLMContext
+from pipecat.processors.aggregators.llm_response_universal import LLMContextAggregatorPair
+from pipecat.processors.frameworks.rtvi import RTVIConfig, RTVIObserver, RTVIProcessor
+from pipecat.runner.types import RunnerArguments
+from pipecat.runner.utils import create_transport
+from pipecat.services.cartesia.tts import CartesiaTTSService
+from pipecat.services.deepgram.stt import DeepgramSTTService
+from pipecat.services.openai.llm import OpenAILLMService
+from pipecat.services.openai.base_llm import BaseOpenAILLMService
+from pipecat.transports.base_transport import BaseTransport, TransportParams
+from pipecat.transports.daily.transport import DailyParams
+
+logger.info("All components loaded successfully!")
+
+load_dotenv(override=True)
+
+# Function to load system prompt from text file
+
+def load_system_prompt(path: str) -> str:
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read().strip()
+
+async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
+    logger.info(f"Starting bot")
+
+    # Speech to Text
+
+    stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"))
+
+    # Text to Speech
+
+    tts = CartesiaTTSService(
+        api_key=os.getenv("CARTESIA_API_KEY"),
+        voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22", 
+    )
+
+    # LLM
+
+    llm = OpenAILLMService(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4.1-nano",
+        params=BaseOpenAILLMService.InputParams(
+            temperature=0.3,            # Response creativity (0.0-2.0)
+            max_completion_tokens=100,  # Maximum response length
+            frequency_penalty=0.2,      # Reduce repetition (0.0-2.0)
+            presence_penalty=0.2,       # Encourage topic diversity (0.0-2.0)
+        ),
+    )
+
+
+    CUSTOM_SYSTEM_PROMPT = load_system_prompt("system_prompt.txt")
+
+    messages = [
+        {
+            "role": "system",
+            "content": CUSTOM_SYSTEM_PROMPT,
+            
+        },
+    ]
+
+    context = LLMContext(messages)
+    context_aggregator = LLMContextAggregatorPair(context)
+
+    rtvi = RTVIProcessor(config=RTVIConfig(config=[]))
+
+    pipeline = Pipeline(
+        [
+            transport.input(),  # Transport user input
+            rtvi,  # RTVI processor
+            stt,
+            context_aggregator.user(),  # User responses
+            llm,  # LLM
+            tts,  # TTS
+            transport.output(),  # Transport bot output
+            context_aggregator.assistant(),  # Assistant spoken responses
+        ]
+    )
+
+    task = PipelineTask(
+        pipeline,
+        params=PipelineParams(
+            enable_metrics=True,
+            enable_usage_metrics=True,
+        ),
+        observers=[RTVIObserver(rtvi)],
+    )
+
+    @transport.event_handler("on_client_connected")
+    async def on_client_connected(transport, client):
+        logger.info(f"Client connected")
+        # Kick off the conversation.
+        messages.append({"role": "system", "content": "Say hello and briefly introduce yourself."})
+        await task.queue_frames([LLMRunFrame()])
+
+    @transport.event_handler("on_client_disconnected")
+    async def on_client_disconnected(transport, client):
+        logger.info(f"Client disconnected")
+        await task.cancel()
+
+    runner = PipelineRunner(handle_sigint=runner_args.handle_sigint)
+
+    await runner.run(task)
+
+
+async def bot(runner_args: RunnerArguments):
+    """Main bot entry point for the bot starter."""
+
+    transport_params = {
+        "daily": lambda: DailyParams(
+            audio_in_enabled=True,
+            audio_out_enabled=True,
+            vad_analyzer=SileroVADAnalyzer(params=VADParams(stop_secs=0.2)),
+            turn_analyzer=LocalSmartTurnAnalyzerV3(),
+        ),
+        "webrtc": lambda: TransportParams(
+            audio_in_enabled=True,
+            audio_out_enabled=True,
+            vad_analyzer=SileroVADAnalyzer(params=VADParams(stop_secs=0.2)),
+            turn_analyzer=LocalSmartTurnAnalyzerV3(),
+        ),
+    }
+
+    transport = await create_transport(runner_args, transport_params)
+
+    await run_bot(transport, runner_args)
+
+
+if __name__ == "__main__":
+    from pipecat.runner.run import main
+
+    main()
